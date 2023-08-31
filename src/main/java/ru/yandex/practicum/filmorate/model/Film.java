98d8e3b97ed2a71dcac6bd8c24450943f74fc0d5
Xan <erensar.the.sorcerer@yandex.ru>
2023-08-31 18:28:52 +0300

    new-new commit

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -12,7 +12,7 @@ import java.time.LocalDate;
 public class Film {
     public Film(@NotNull @NonNull String name, String description, LocalDate releaseDate, int duration) {
         this.name = name;
-        this.description =description;
+        this.description = description;
         this.releaseDate = releaseDate;
         this.duration = duration;
     }
@@ -25,7 +25,7 @@ public class Film {
     @NotNull
     @NonNull
     @NotBlank private String name;
-    @Size(max=200) private String description;
+    @Size(max = 200) private String description;
     private LocalDate releaseDate;
     private int duration;
     private int rate;
